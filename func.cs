7f08c074b3237d2b3e7fc59398724722b15b4a88
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-04-21 21:41:32 +0300

    Добавьте файлы проекта.

---
+++ func.cs
@@ -0,0 +1,31 @@
+﻿
+namespace func
+{
+    internal class func
+    {
+
+        static double CalculateSquareRoot(double targett)
+        {
+            double x = 1;
+            double oldx;
+
+            do
+            {
+                oldx = x;
+                x = (x + targett / x) / 2;
+            } while (oldx != x);
+
+            return x;
+
+        }
+        static void Main(string[] args)
+        {
+
+            double target = 2023;
+            CalculateSquareRoot(target);
+
+            Console.WriteLine($"x = {CalculateSquareRoot(target)}");
+            Console.WriteLine($"x^2 = {Math.Round(Math.Pow(CalculateSquareRoot(target), 2))}");
+        }
+    }
+}
